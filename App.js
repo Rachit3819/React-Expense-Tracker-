8857f303a34eefb0e912073ec1a5d31a2cf2683b
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-09 17:29:43 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,37 @@
-import ExpenceItem from "./Component/ExpenceItem";
-
-const App=()=> {
-  const Expense = [
+import React from "react";
+import Expenses from "./Component/Expenses/Expenses";
+import NewExpense from "./Component/Expenses/NewExpenses/NewExpense";
+const App = () => {
+  const expenses = [
     {
-      id: "a1",
-      LocationOfExpenditure: "Agra",
-      Title: "CarInsaurance",
-      Amount: 10345,
-      date: new Date(2023, 1, 31),
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "b2",
-      LocationOfExpenditure: "New Delhi",
-      Title: "Petrol",
-      Amount: 1000,
-      date: new Date(2022, 12, 20),
-    },
-    {
-      id: "c3",
-      LocationOfExpenditure: "Nodia",
-      Title: "Lunch",
-      Amount: 836,
-      date: new Date(2023, 1, 17),
-    },
-    {
-      id: "c4",
-      LocationOfExpenditure: "Jaipur",
-      Title: "AmusementPark",
-      Amount: 1200,
-      date: new Date(2023, 1, 31),
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler=expense=>{
+    console.log(expense)
+  }
+
   return (
-    <div className="App">
-      {Expense.map((element, i) => (
-        <h1>
-          <ExpenceItem
-            Title={element.Title}
-            Amount={element.Amount}
-            date={element.date}
-            LocationOfExpenditure={element.LocationOfExpenditure}
-            id={element.id}
-          ></ExpenceItem>
-        </h1>
-      ))}
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
-
+};
 export default App;
