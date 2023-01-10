3c0f02462cf972a3facda188a20cf858aa3515b8
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-11 01:05:01 +0530

    Add files via upload

--- Expenses.js
+++ Expenses.js
@@ -4,13 +4,25 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./NewExpenses/ExpenseFilter";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  const filterExpense=props.items.filter(expense=>{
-    return expense.date.getFullYear().toString()===filteredYear
-  })
+  const filterExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent = <p> Only single Expense here. Please add more</p>;
+  if (filterExpense.length > 1) {
+     filterExpense.map((expenses) => (
+      <ExpenseItem
+        key={expenses.id}
+        title={expenses.title}
+        amount={expenses.amount}
+        date={expenses.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,16 +30,10 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filterExpense.map((expenses) => (
-          <ExpenseItem
-          key={expenses.id}
-            title={expenses.title}
-            amount={expenses.amount}
-            date={expenses.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
 };
+
 export default Expenses;
