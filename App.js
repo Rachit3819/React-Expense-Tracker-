7d9c4d028a902f8edd710d8ffe2b228d5ea0e8df
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-10 23:12:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
 import Expenses from "./Component/Expenses/Expenses";
 import NewExpense from "./Component/Expenses/NewExpenses/NewExpense";
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "Iphone 14",
+    amount: 1250,
+    date: new Date(2023, 1, 11),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses,setExpenses]=useState(Dummy_Expenses)
   const addExpenseHandler=expense=>{
-    console.log(expense)
+    setExpenses((previousExpense)=>{
+      return [expense,...previousExpense]
+    })
   }
-
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
